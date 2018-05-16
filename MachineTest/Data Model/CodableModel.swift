017f53cac21457456fabf5b6e31640d7430abf7c
ajurav1 <30289821+ajurav1@users.noreply.github.com>
2018-05-16 15:58:14 +0530

    Add files via upload

---
+++ MachineTest/Data Model/CodableModel.swift
@@ -0,0 +1,36 @@
+//
+//  ItemDataModel.swift
+//  MachineTest
+//
+//  Created by Ajay Walia on 05/05/18.
+//  Copyright © 2018 mac min . All rights reserved.
+
+
+import Foundation
+protocol CodableModel: Codable{
+    associatedtype dataModel
+    static func getDataModel(_ jsonData: Data) -> dataModel?
+    func getJsonData() -> Data?
+}
+extension CodableModel{
+    static func getDataModel(_ jsonData: Data) -> dataModel?{
+        let jsonDecoder = JSONDecoder()
+        if let apiResponse = try? jsonDecoder.decode(self, from: jsonData){
+            if let apiData = apiResponse as? Self.dataModel{
+                return apiData
+            }
+        }else{
+            if let jsonDict = try? JSONSerialization.jsonObject(with: jsonData, options: .allowFragments) as! NSDictionary{
+                print(jsonDict) //To see unexpected json data
+            }
+        }
+        return nil
+    }
+    func getJsonData() -> Data? {
+        let jsonEncoder = JSONEncoder()
+        if let data = try? jsonEncoder.encode(self){
+            return data
+        }
+        return nil
+    }
+}
